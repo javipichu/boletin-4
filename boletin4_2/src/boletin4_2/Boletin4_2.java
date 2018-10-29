afadabfa34e09d15c80d37190073868e6d54c3e8
jalvarezotero <you@example.com>
2018-10-29 12:12:01 +0100

    ACABADO

---
+++ boletin4_2/src/boletin4_2/Boletin4_2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin4_2;
+
+/**
+ *
+ * @author jalvarezotero
+ */
+
+    public class Boletin4_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Satelite satelite1 = new Satelite(6f,2f,5f);
+        satelite1.verPosicion();
+        
+    }
+    
+}
